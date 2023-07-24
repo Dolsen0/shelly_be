231115cfc5829fb7698ce22ccf0c07527dad78df
dolsen0 <derek.olsen@gmail.com>
2023-07-24 16:01:00 -0400

    accessing api from backend

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/home', async (req, res) => {
   try {
     const response = await axios.get('http://192.168.33.1/rpc/Shelly.GetStatus');
-    res.send(response.data);
+    res.json(response.data);
   } catch (error) {
     res.status(500).send({ error: 'Error fetching status' });
   }
