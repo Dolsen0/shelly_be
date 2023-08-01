d7b5dbe214421630a972da77db3d47a71ee6a348
dolsen0 <derek.olsen@gmail.com>
2023-08-01 13:53:46 -0400

    added environment variables

--- dbConnect.js
+++ dbConnect.js
@@ -1,28 +1,29 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import password from './password.json' assert { type: 'json' };
+import dotenv from "dotenv";
+dotenv.config();
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = `mongodb+srv://derekolsen:${password.password}@cluster0.vs0nwmu.mongodb.net/?retryWrites=true&w=majority`;
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@cluster0.vs0nwmu.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 const run = async () => {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
-}
+};
 
 run().catch(console.dir);
