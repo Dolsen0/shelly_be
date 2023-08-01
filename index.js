1cbc1a22e074f043e2b253716989d5b9b98beda8
dolsen0 <derek.olsen@gmail.com>
2023-08-01 13:38:37 -0400

    created endpoint to view data in collection

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
-import axios from 'axios';
-import express from 'express';
-import cors from 'cors';
-import client from './dbConnect.js';
-import dotenv from 'dotenv'
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+import { client } from "./dbConnect.js";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 
-dotenv.config()
+dotenv.config();
 
 app.use(cors());
 
-app.get('/home', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const response = await axios.get('http://192.168.15.196/rpc/Shelly.GetStatus');
-    res.json(response.data);
-  } 
-  catch (error) {
-    res.status(500).json({ error: 'Error fetching status' });
+    await client.connect();
+    const db = client.db("shelly");
+    const collection = db.collection("device");
+    const data = await collection.find().toArray();
+    res.json(data);
+  } catch (error) {
+    console.error("Error while fetching data from MongoDB: ", error);
+    res.status(500).json({ error: "Error fetching data from MongoDB" });
+  } finally {
+    await client.close();
   }
 });
 
-app.get('/home/restart', async(req, res) => {
+app.get("/home", async (req, res) => {
   try {
-    const response = await axios.get('http://192.168.33.1/rpc/Switch.Toggle?id=0');
+    const response = await axios.get(
+      "http://192.168.15.196/rpc/Shelly.GetStatus"
+    );
     res.json(response.data);
-  } 
-  catch(error) {
-    res.status(500).json({error: 'Error fetching status'});
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching status" });
   }
-})
+});
 
-app.get('/home/light', async(req, res) => {
-  try{
-    const response = await axios.get('http://192.168.15.100/light/0?brightness=50')
+app.get("/home/restart", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "http://192.168.33.1/rpc/Switch.Toggle?id=0"
+    );
     res.json(response.data);
-  } 
-  catch(error) {
-    res.status(500).json({error: 'Error'})
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching status" });
   }
-})
+});
+
+app.get("/home/light", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "http://192.168.15.100/light/0?brightness=50"
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
