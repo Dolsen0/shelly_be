715ef30f64a2b2f59fa977d61328709f5463167f
dolsen0 <derek.olsen@gmail.com>
2023-08-02 01:11:47 -0400

    Added 10 minute cron job to update database

--- index.js
+++ index.js
@@ -3,14 +3,40 @@ import express from "express";
 import cors from "cors";
 import { client } from "./dbConnect.js";
 import dotenv from "dotenv";
+import cron from "node-cron";
 import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 
 const app = express();
 const port = 3000;
 
 dotenv.config();
-
 app.use(cors());
+
+const fixedId = new ObjectId("60e2b171044f1a2768a740b0");
+
+async function updateDeviceState() {
+  const response = await axios.get("http://192.168.0.2/rpc/Shelly.GetStatus");
+  await client.connect();
+  const db = client.db("shelly");
+  const collection = db.collection("device");
+  await collection.updateOne(
+    { _id: fixedId },
+    { $set: response.data },
+    { upsert: true }
+  );
+  const updatedDoc = await collection.findOne({ _id: fixedId });
+  await client.close();
+  return updatedDoc;
+}
+
+cron.schedule("*/10 * * * *", async () => {
+  console.log("Updating device state...");
+  try {
+    await updateDeviceState();
+  } catch (error) {
+    console.error("Error in cron job: ", error);
+  }
+});
 
 app.get("/", async (req, res) => {
   try {
